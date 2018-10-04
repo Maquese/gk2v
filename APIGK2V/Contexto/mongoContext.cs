8b60b34c92dfb7132272a9e3c038f4f8baec384f
Your Name <kenneymaquese@gmail.com>
2018-10-04 16:34:41 -0300

    connectado e com as collections no banco GK2v

--- APIGK2V/Contexto/mongoContext.cs
+++ APIGK2V/Contexto/mongoContext.cs
@@ -11,10 +11,9 @@ namespace APIGK2V.Contexto
         public mongoContext()
         {
             //jvarb.gcp.mongodb.net/test cluster vinicius
+            //vmobr-shard-00-00-jvarb.gcp.mongodb.net:27017
            mongo  = new MongoClient("mongodb://admin:admin@cluster0-shard-00-00-xhygg.mongodb.net:27017,cluster0-shard-00-01-xhygg.mongodb.net:27017,cluster0-shard-00-02-xhygg.mongodb.net:27017?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true").GetDatabase("GK2V");
-
             //mongo = new MongoClient().GetDatabase("test");
-           //var x = mongo.GetCollection<Cup>("Cup").Find("{}").ToList();
         }
 
         public IMongoDatabase contextoAtual()
