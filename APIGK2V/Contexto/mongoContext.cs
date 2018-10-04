db5fd9be6e6edae0f4e84c8b4ab672e7c2167649
unknown <kenneymaquese@gmail.com>
2018-10-04 00:55:02 -0300

    conectando com o cluester

--- APIGK2V/Contexto/mongoContext.cs
+++ APIGK2V/Contexto/mongoContext.cs
@@ -1,4 +1,5 @@
 using APIGK2V.Contratos;
+using APIGK2V.Entidades;
 using MongoDB.Driver;
 
 namespace APIGK2V.Contexto
@@ -9,8 +10,11 @@ namespace APIGK2V.Contexto
 
         public mongoContext()
         {
-            mongo = new MongoClient().GetDatabase("test");
-            var x = mongo.ListCollections();
+            //jvarb.gcp.mongodb.net/test cluster vinicius
+           mongo  = new MongoClient("mongodb://admin:admin@cluster0-shard-00-00-xhygg.mongodb.net:27017,cluster0-shard-00-01-xhygg.mongodb.net:27017,cluster0-shard-00-02-xhygg.mongodb.net:27017?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true").GetDatabase("GK2V");
+
+            //mongo = new MongoClient().GetDatabase("test");
+           //var x = mongo.GetCollection<Cup>("Cup").Find("{}").ToList();
         }
 
         public IMongoDatabase contextoAtual()
